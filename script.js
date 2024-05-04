11b88e45724ca24d6ad82e2aaa216ec40c27d190
kyotodelflor <159265843+kyotodelflor@users.noreply.github.com>
2024-05-04 20:26:29 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,50 @@
-const btWiki = document.querySelector('.button__secondary')
-const randomChrBt = document.getElementById('try-it')
-const btHomepage = document.getElementById('bt_homepage')
-async function fetchData() {
-    try {
-        const response = await fetch('http://gateway.marvel.com/v1/public/characters?ts=1714403935663&apikey=66335a5d3fde2c8e7b0e533b6ea5c549&hash=16b419b0d742aaf856f4696518ceee0c');
-        const data = await response.json();
+// async function Marvel() {
+//     await fetch(`http://gateway.marvel.com/v1/public/comics?ts=1714403935663&apikey=66335a5d3fde2c8e7b0e533b6ea5c549&hash=16b419b0d742aaf856f4696518ceee0c`)
+//         .then((response) => response.json())
+//         .then(data => arr1 = data)
+//     console.log(arr1.data.results);
 
-        let charactersArr = data.data.results
-        let randomKeyObjCharacters = charactersArr[Math.floor(Math.random() * charactersArr.length)]
-        let randomKeyObjChrValue = randomKeyObjCharacters
-        if (randomKeyObjChrValue !== undefined && randomKeyObjChrValue !== null) {
-            generateCharacters(randomKeyObjChrValue);
-        } else {
-            console.log('randomKeyObjChrValue is null or undefined');
-        }
-        randomCharactersWiki(randomKeyObjChrValue.urls);
-        generatebtHomepage(randomKeyObjChrValue.urls)
-    } catch (error) {
-        console.error('Ошибка при загрузке данных:', error);
+//     divWithInfo.innerHTML += `
+//     `
+// }
+
+// Marvel()
+
+async function displayComics() {
+    try {
+        const response = await fetch('http://gateway.marvel.com/v1/public/comics?limit=20&ts=1714403935663&apikey=66335a5d3fde2c8e7b0e533b6ea5c549&hash=16b419b0d742aaf856f4696518ceee0c');
+        const data = await response.json();
+        const comics = data.data.results;
+        const comicsContainer = document.querySelector('.comics__grid');
+
+        let count = 0;
+        let rowHTML = '';
+
+        comics.forEach((comic, index) => {
+
+            rowHTML += `
+                <li class="comics__item">
+                    <a href="#">
+                        <img src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}" class="comics__item-img">
+                        <div class="comics__item-name">${comic.title}</div>
+                        <div class="comics__item-price">${comic.prices[0].price}$</div>
+                    </a>
+                </li>
+            `;
+
+            count++;
+
+            if (count === 4 || index === comics.length - 1) {
+
+                comicsContainer.innerHTML += `${rowHTML}`;
+
+                count = 0;
+                rowHTML = '';
+            }
+        });
+    } catch {
+      
     }
 }
 
-// Random Characters generate
-const generateCharacters = (randomKeyObjChrValue) => {
-    const randomcharImg = document.querySelector('.randomchar__img')
-    const randomcharName = document.querySelector('.randomchar__name')
-    const randomcharDescr = document.querySelector('.randomchar__descr')
-    const srcCharactersImg = randomKeyObjChrValue.thumbnail.path + '.' + randomKeyObjChrValue.thumbnail.extension
-    const charactersDesc = randomKeyObjChrValue.description.length == 0 ? 'UNKNOWN' : randomKeyObjChrValue.description
-    randomcharImg.src = ''
-    randomcharName.innerText = ''
-    randomcharDescr.innerText = ''
-    randomcharImg.src = `${srcCharactersImg}`
-    randomcharName.innerText = `${randomKeyObjChrValue.name}`
-    randomcharDescr.innerText = `${charactersDesc}`
-}
-
-// Random Character wiki URL
-const randomCharactersWiki = (randomUrlWiki) => {
-    let urlWiki = randomUrlWiki
-    let charactersWiki = ''
-    urlWiki.forEach(elementWiki => {
-        if (elementWiki.type == 'wiki') {
-            charactersWiki = elementWiki.url
-        }
-    });
-    btWiki.addEventListener('click', () => {
-        btWiki.href = `${charactersWiki}`
-    })
-}
-
-// Random Character homepage URL
- const generatebtHomepage = (homepageUrl) => {
-    let url = ''
-    console.log(homepageUrl);
-    homepageUrl.forEach(elHomepage => {
-        if (elHomepage.type == 'detail') {
-            url = elHomepage.url
-        }
-    })
-    btHomepage.addEventListener('click', () => {
-        btHomepage.href = `${url}`
-    })
- }
-
-// Random Character
-randomChrBt.addEventListener('click', () => {
-    fetchData();
-})
-
-fetchData();
+displayComics();
