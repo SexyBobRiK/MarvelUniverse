89b43c4b7a5d4ac74d6b50765530c829838cf83e
s3AKAT <168458651+s3AKAT@users.noreply.github.com>
2024-05-04 20:44:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const api =
-  "https://gateway.marvel.com:443/v1/public/characters?limit=100&apikey=72c811efbcd934c114570bb4bf01a85d";
-fetch(api)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
 
-const charItem = document.querySelector(".char__item");
-charItem.addEventListener("click", async function () {
-  await fetch(
-    "http://gateway.marvel.com/v1/public/comics?ts=1714403935663&apikey=66335a5d3fde2c8e7b0e533b6ea5c549&hash=16b419b0d742aaf856f4696518ceee0c"
-  )
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-  console.log(charItem);
-});
 
 const main = document.querySelector("#characters-for-script");
 const app = document.querySelector("#comics-for-script");
